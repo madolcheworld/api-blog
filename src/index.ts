84f39a0c47462c0c313f52c3e529f1ac2f7f85b7
madolche <madolcheworld@gmail.com>
2024-08-14 19:34:15 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,6 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/users', userrouter)
-app.get('/test', (req, res) => {
-  const data = {
-    nama : 1,
-    username : 1,
-    password : 2
-  }
-  const {error} = userSchema.validate(data)
-  if(error) {
-    
-  return res.status(400).json(error.message)
-  }
-})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
