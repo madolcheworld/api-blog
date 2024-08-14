919356c0be345553e59dd4c153ddf9449983c090
madolche <madolcheworld@gmail.com>
2024-08-14 13:53:21 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userrouter from './router/user-router';
 dotenv.config();
 
 
@@ -7,7 +8,7 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-
+app.use('/users', userrouter)
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
