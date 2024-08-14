4d3af567204af18f8b1f3733f771474365c61ae0
madolche <madolcheworld@gmail.com>
2024-08-14 19:33:09 +0700

    ok

--- dist/index.js
+++ dist/index.js
@@ -5,12 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 var user_router_1 = __importDefault(require("./router/user-router"));
+dotenv_1.default.config();
 var app = (0, express_1.default)();
 var port = 3000;
 app.use(express_1.default.json());
-app.use(user_router_1.default);
+app.use('/users', user_router_1.default);
 app.listen(port, function () {
     console.log("Example app listening at http://localhost:".concat(port));
 });
